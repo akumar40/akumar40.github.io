c6c50235915b9222a6d05c5d006ae2e7428a38b5
Avinash Kumar <akumar40@stevens.edu>
2022-01-26 17:52:12 -0500

    Initial Commit

---
+++ src/app/welcome-page/welcome-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.scss']
+})
+export class WelcomePageComponent implements OnInit {
+
+  constructor() { }
+  firstLogoActive: boolean = true;
+
+  ngOnInit(): void {
+  }
+
+  public invertLogo() {
+    this.firstLogoActive = !this.firstLogoActive;
+  }
+
+}
